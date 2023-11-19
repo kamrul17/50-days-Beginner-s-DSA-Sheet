ea515311af9a1d0b671bb85a1d835e9f6adecaf3
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2023-11-19 19:33:16 +0530

    solved 1 BDSA SHEET

---
+++ rotate_Matrix_by_90_antiClockwise.cpp
@@ -0,0 +1,52 @@
+// ROTATE image BY 90 DEGREE
+
+#include <iostream>
+using namespace std;
+void rotateMatrix(int matrix[][3], int n)
+{
+    //   transpose
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    // reverse
+    for (int i = 0; i < n / 2; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - i][j];
+            matrix[n - 1 - i][j] = temp;
+            // matrix[i][j] = matrix[n - 1][j];
+            // matrix[n - 1][j] = temp;
+        }
+    }
+}
+void printmat(int matrix[][3], int n)
+{
+    //   transpose
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    // int matrix[5][5] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7};
+    int matrix[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printmat(matrix, 3);
+    cout << endl;
+    rotateMatrix(matrix, 3);
+    printmat(matrix, 3);
+
+    return 0;
+}
